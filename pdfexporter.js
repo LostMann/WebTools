b221f9944a4051ddd50ba3734bfcc7c5bbca51ce
Sedat Sarıgül <s_sarigul@hotmail.com>
2021-07-22 18:54:34 +0300

    Update pdfexporter.js

--- pdfexporter.js
+++ pdfexporter.js
@@ -30,31 +30,32 @@
 	}
 }( typeof global !== "undefined" ? global : this ));
 
+var doc ={};
 myloadJS('https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.2.61/jspdf.min.js',function(){
 	console.log('PDF JS loaded!');
 	
-	var doc = new jsPDF();
-
-	function saveDiv(divId, title) {
-	 doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
-	 doc.save('div.pdf');
-	}
-
-	function printDiv(divId, title, height, width, top, left) {
-	  height=height || 650; width=width || 900;top=top || 100;left=left || 150;  
-	  let mywindow = window.open('', 'PRINT', 'height='+height+',width='+width+',top='+top+',left='+left);
-
-	  mywindow.document.write(`<html><head><title>${title}</title>`);
-	  mywindow.document.write('</head><body >');
-	  mywindow.document.write(document.getElementById(divId).innerHTML);
-	  mywindow.document.write('</body></html>');
-
-	  mywindow.document.close(); // necessary for IE >= 10
-	  mywindow.focus(); // necessary for IE >= 10*/
-
-	  mywindow.print();
-	  mywindow.close();
-
-	  return true;
-	}	
+	doc = new jsPDF();	
 });
+
+function saveDiv(divId, title) {
+ doc.fromHTML(`<html><head><title>${title}</title></head><body>` + document.getElementById(divId).innerHTML + `</body></html>`);
+ doc.save('div.pdf');
+}
+
+function printDiv(divId, title, height, width, top, left) {
+  height=height || 650; width=width || 900;top=top || 100;left=left || 150;  
+  let mywindow = window.open('', 'PRINT', 'height='+height+',width='+width+',top='+top+',left='+left);
+
+  mywindow.document.write(`<html><head><title>${title}</title>`);
+  mywindow.document.write('</head><body >');
+  mywindow.document.write(document.getElementById(divId).innerHTML);
+  mywindow.document.write('</body></html>');
+
+  mywindow.document.close(); // necessary for IE >= 10
+  mywindow.focus(); // necessary for IE >= 10*/
+
+  mywindow.print();
+  mywindow.close();
+
+  return true;
+}
